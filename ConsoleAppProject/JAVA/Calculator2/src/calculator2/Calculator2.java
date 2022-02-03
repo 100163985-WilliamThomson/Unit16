be75eaa3e8422ebe97ac9aacef29ea76efcb1178
William Thomson <100163985@student.burnley.ac.uk>
2022-02-03 14:42:49 +0000

    Created a basic calculator and driver main.

--- ConsoleAppProject/JAVA/Calculator2/src/calculator2/Calculator2.java
+++ ConsoleAppProject/JAVA/Calculator2/src/calculator2/Calculator2.java
@@ -2,19 +2,45 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
 package calculator2;
 
 /**
  *
  * @author 100163985
  */
-public class Calculator2 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class calculator2 
+        
+{
+    String name;
+    
+    public int add(int x,int y) 
+    {
+        int z = x+y;
+        return z;
     }
     
+    public int subtract(int x,int y) 
+    {
+        int z = x-y;
+        return z;
+    }
+    public int multiply(int x,int y) 
+    {
+        int z = x*y;
+        return z;
+    }
+    
+    public int divide(int x,int y) 
+    {
+        int z = x/y;
+        return z;
+    }
+    
+    public int modulo(int x,int y) 
+    {
+        int z;
+        z = x % y;
+        return z;
+    }
 }
---
+++ ConsoleAppProject/JAVA/Calculator2/src/calculator2/main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculator2;
+
+/**
+ *
+ * @author 100163985
+ */
+public class main 
+{
+    public static void main(String[] args)
+    {
+        calculator2 c1 = new calculator2();
+        
+        c1.name = "That one I spent £25 on";
+        System.out.println(c1.modulo(3, 5));
+        System.out.println(c1.name);
+        
+    }
+}
